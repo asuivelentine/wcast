fedcd632b8f5283631f74b7dc3ac02a08f8c0345
Kai Sickeler <k.sickeler@gmail.com>
2016-02-12 16:48:58 +0100

    beautified json test input

--- src/winfo.rs
+++ src/winfo.rs
@@ -1,6 +1,6 @@
 use std::convert::From;
 
-use rustc_serialize::json::Json;
+use rustc_serialize::json::{ Json, Object };
 
 pub type Time = u64;
 
@@ -197,23 +197,54 @@ mod tests {
     #[test]
     fn test_country_forecast() {
         let json = 
-        "{\"city\":{\"id\":524901,\"name\":\"Moscow\",\"coord\":{\"lon\":37.615555,\"lat\":55.75222},\"country\":\"RU\",
-        \"population\":0,\"sys\":{\"population\":0}},\"cod\":\"200\",\"message\":0.008,\"cnt\":37,\"list\":[
-        {\"dt\":1455278400,\"main\":{\"temp\":274.089,\"temp_min\":274.089,\"temp_max\":274.089,\"pressure\":1009.52,
-        \"sea_level\":1030.13,\"grnd_level\":1009.52,\"humidity\":96,\"temp_kf\":0},\"weather\":[{\"id\":500,
-        \"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10n\"}],\"clouds\":{\"all\":92},
-        \"wind\":{\"speed\":4.82,\"deg\":270.001},\"rain\":{\"3h\":0.17},\"snow\":{\"3h\":0.05},\"sys\":{\"pod\":\"n\"},
-        \"dt_txt\":\"2016-02-16 21:00:00\"},{\"dt\":1455667200,\"main\":{\"temp\":273.583,\"temp_min\":273.583,
-        \"temp_max\":273.583,\"pressure\":1010.05,\"sea_level\":1030.65,\"grnd_level\":1010.05,\"humidity\":97,
-        \"temp_kf\":0},\"weather\":[{\"id\":600,\"main\":\"Snow\",\"description\":\"light snow\",\"icon\":\"13n\"}],
-        \"clouds\":{\"all\":92},\"wind\":{\"speed\":5.01,\"deg\":273.501},\"rain\":{},\"snow\":{\"3h\":0.12},
-        \"sys\":{\"pod\":\"n\"},\"dt_txt\":\"2016-02-17 00:00:00\"}]}";
+        "{\"city\":{\"id\":524901,\"name\":\"Moscow\",\"coord\":{\"lon\":37.615555,\"lat\":55.75222},
+        \"country\":\"RU\",\"population\":0,\"sys\":{\"population\":0}},\"cod\":\"200\",\"message\":
+        0.008,\"cnt\":37,\"list\":[{\"dt\":1455278400,\"main\":{\"temp\":274.089,\"temp_min\":
+        274.089,\"temp_max\":274.089,\"pressure\":1009.52,\"sea_level\":1030.13,\"grnd_level\":
+        1009.52,\"humidity\":96,\"temp_kf\":0},\"weather\":[{\"id\":500,\"main\":\"Rain\",
+        \"description\":\"light rain\",\"icon\":\"10n\"}],\"clouds\":{\"all\":92},\"wind\":{\"speed
+        \":4.82,\"deg\":270.001},\"rain\":{\"3h\":0.17},\"snow\":{\"3h\":0.05},\"sys\":{\"pod\":\"n\"},
+        \"dt_txt\":\"2016-02-16 21:00:00\"},{\"dt\":1455667200,\"main\":{\"temp\":273.583,
+        \"temp_min\":273.583,\"temp_max\":273.583,\"pressure\":1010.05,\"sea_level\":1030.65,
+        \"grnd_level\":1010.05,\"humidity\":97,\"temp_kf\":0},\"weather\":[{\"id\":600,\"main\":
+        \"Snow\",\"description\":\"light snow\",\"icon\":\"13n\"}],\"clouds\":{\"all\":92},\"wind\":
+        {\"speed\":5.01,\"deg\":273.501},\"rain\":{},\"snow\":{\"3h\":0.12},\"sys\":{\"pod\":\"n\"},
+        \"dt_txt\":\"2016-02-17 00:00:00\"}]}";
 
         let json = Json::from_str(&json).unwrap();
         let country= WeatherInfo::get_country(json);
 
         assert!(country.is_some());
         assert_eq!("RU", country.unwrap()) 
+    }
+
+    #[test]
+    fn test_city_forecast() {
+        let json = 
+        "{\"city\":{\"id\":524901,\"name\":\"Moscow\",\"coord\":{\"lon\":37.615555,\"lat\":55.75222},
+        \"country\":\"RU\",\"population\":0,\"sys\":{\"population\":0}},\"cod\":\"200\",\"message\":
+        0.008,\"cnt\":37,\"list\":[{\"dt\":1455278400,\"main\":{\"temp\":274.089,\"temp_min\":
+        274.089,\"temp_max\":274.089,\"pressure\":1009.52,\"sea_level\":1030.13,\"grnd_level\":
+        1009.52,\"humidity\":96,\"temp_kf\":0},\"weather\":[{\"id\":500,\"main\":\"Rain\",
+        \"description\":\"light rain\",\"icon\":\"10n\"}],\"clouds\":{\"all\":92},\"wind\":{\"speed
+        \":4.82,\"deg\":270.001},\"rain\":{\"3h\":0.17},\"snow\":{\"3h\":0.05},\"sys\":{\"pod\":\"n\"},
+        \"dt_txt\":\"2016-02-16 21:00:00\"},{\"dt\":1455667200,\"main\":{\"temp\":273.583,
+        \"temp_min\":273.583,\"temp_max\":273.583,\"pressure\":1010.05,\"sea_level\":1030.65,
+        \"grnd_level\":1010.05,\"humidity\":97,\"temp_kf\":0},\"weather\":[{\"id\":600,\"main\":
+        \"Snow\",\"description\":\"light snow\",\"icon\":\"13n\"}],\"clouds\":{\"all\":92},\"wind\":
+        {\"speed\":5.01,\"deg\":273.501},\"rain\":{},\"snow\":{\"3h\":0.12},\"sys\":{\"pod\":\"n\"},
+        \"dt_txt\":\"2016-02-17 00:00:00\"}]}";
+
+
+        let json = Json::from_str(&json).unwrap();
+        let city= WeatherInfo::get_city(json);
+
+        assert!(city.is_some());
+        let city = city.unwrap();
+        assert_eq!("Moscow", city.name);
+        assert_eq!("RU", city.country);
+        assert_eq!(55.75222, city.lat);
+        assert_eq!(37.615555, city.lng);
     }
 
     #[test]
